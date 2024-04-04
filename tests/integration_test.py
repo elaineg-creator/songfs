65c89d6204a7a9bb27bd02b130963a7a58fa0264
Tim G <tim.d.geissler@gmail.com>
2024-04-04 11:12:36 -0400

    integration test full FS

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -3,6 +3,13 @@ import subprocess
 import time
 import unittest
 
+##############################################################
+#                                                            #
+#   Brings up SongFS and compares output to filepaths.txt    #
+#                                                            #
+#                                                            #
+##############################################################
+
 # Define the directory containing the MP3 files
 mp3_directory = os.path.abspath("./pyFUSE/rootdir")
 
@@ -38,13 +45,59 @@ def check_root(output):
     unittest.TestCase().assertListEqual(sorted(actual_directories), sorted(expected_root))
     print("Root dir PASSED")
 
+# Update expected file
+# def update_expected():
+#      with open(filepaths_data, "a") as f:
+#             for root, dirs, files in os.walk(mount_point):
+#                 for file in files:
+#                     full_path = os.path.join(root, file)
+#                     rel_path = os.path.relpath(full_path, start=mount_point)
+#                     print(f"Found file: {rel_path}")
+#                     # Append the relative path of the file to the text file
+#                     f.write(rel_path + "\n")
 
 try:
     # Perform operations on the mounted FS
     print("Listing contents of the mount point:")
-    output = subprocess.check_output(["ls", "-l", mount_point])
-    print(output.decode("utf-8"))
-    check_root(output)
+
+    # Initialize list of filepaths
+    filepaths_data = "tests/filepaths.txt"
+    files_data = []
+
+    count = 0
+    total = 0
+
+    with open(filepaths_data, "r") as file:
+        # Read each line and append it to the file_paths list
+        for line in file:
+            files_data.append(line.strip())
+
+    print("SCANNED COMPARISON DATA")
+    for ln in files_data:
+        print("\t" + ln)
+
+    for root, dirs, files in os.walk(mount_point):
+        for file in files:
+            full_path = os.path.join(root, file)
+            rel_path = os.path.relpath(full_path, start=mount_point)
+            print(f"Found file: {rel_path}")
+            total+=1
+
+            if rel_path in files_data:
+                # Remove the rel_path from the file_paths list
+                files_data.remove(rel_path)
+                count+=1
+                print("FILE FOUND (" + str(count) + "/" + str(total) + ")")
+            else:
+                # If rel_path doesn't exist in file_paths, throw an assertion error
+                assert False, f"File not found: {rel_path}"
+
+    
+    # output = subprocess.check_output(["ls", "-l", mount_point])
+    # print(output.decode("utf-8"))
+    # check_root(output)
+    assert not files_data, "Some files were not found during the scan."
+    print("PASSED ALL TESTS (" + str(count) + "/" + str(total) + ")")
 
 finally:
     # Clean up & terminate songfs process
