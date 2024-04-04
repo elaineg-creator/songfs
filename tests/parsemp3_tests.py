7eaa6f8dfd2e1161d8f5d675cff308417728f895
Elaine Guo <elaine.guo@duke.edu>
2024-04-04 10:34:45 -0400

    fixed parsemp3 tests

--- tests/parsemp3_tests.py
+++ tests/parsemp3_tests.py
@@ -6,14 +6,14 @@ import os
 
 
 # Import the script you want to test
-sys.path.append('../songfs')
+sys.path.append('../pyFUSE')
 import parsemp3
 
 class TestScript(unittest.TestCase):
     
     def test_get_dictionary(self):
         # Test cases for function1
-        path = os.path.normpath(os.getcwd() + "/../mp3_directory/parsemp3_test_songs").replace('\\','/')
+        path = os.path.normpath(os.getcwd() + "/../songfs/mp3_directory/parsemp3_test_songs").replace('\\','/')
         print(path)
         song_dict, song_path_dict = parsemp3.generate_music_library(path)
         parsemp3.print_music_library(song_dict, song_path_dict)
@@ -25,8 +25,8 @@ class TestScript(unittest.TestCase):
         self.assertEqual(song_dict["john s"], {'Unknown Album': ['always with me always with you.mp3']})
         self.assertEqual(song_dict["Unknown Artist"], {'one': ['happy summer long.mp3']})
         self.assertEqual(song_dict["Taylor Swift"], {'1989': ['Bad Blood.mp3', 'Taylor Swift - Is It Over Now_ (Taylor\'s Version) (From The Vault) (Lyric Video) (64 kbps).mp3'], 'Midnights': ["You're Losing Me.mp3"]})
-        self.assertEqual(song_path_dict["always with me always with you.mp3"], os.path.normpath(os.getcwd() + "/../mp3_directory/parsemp3_test_songs/always-with-me-always-with-you-long-21256.mp3"))
-        self.assertEqual(song_path_dict["Taylor Swift - Is It Over Now_ (Taylor's Version) (From The Vault) (Lyric Video) (64 kbps).mp3"], os.path.normpath(os.getcwd() + "/../mp3_directory/parsemp3_test_songs/Taylor Swift - Is It Over Now_ (Taylor's Version) (From The Vault) (Lyric Video) (64 kbps).mp3"))
+        self.assertEqual(song_path_dict["always with me always with you.mp3"], os.path.normpath(os.getcwd() + "/../songfs/mp3_directory/parsemp3_test_songs/always-with-me-always-with-you-long-21256.mp3"))
+        self.assertEqual(song_path_dict["Taylor Swift - Is It Over Now_ (Taylor's Version) (From The Vault) (Lyric Video) (64 kbps).mp3"], os.path.normpath(os.getcwd() + "/../songfs/mp3_directory/parsemp3_test_songs/Taylor Swift - Is It Over Now_ (Taylor's Version) (From The Vault) (Lyric Video) (64 kbps).mp3"))
     
 
 if __name__ == '__main__':
