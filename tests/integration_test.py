9b71f629329873d3ed862282fcdadd3a2bd714d1
Tim G <tim.d.geissler@gmail.com>
2024-04-04 00:04:26 -0400

    fixed comments

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -14,14 +14,14 @@ print("Mounting " + mp3_directory + " to " + mount_point)
 # Start songfs in the background
 proc = subprocess.Popen(["python3", "songfs.py", mp3_directory, mount_point])
 
-# Allow some time for songfs to initialize
+# Assume 2sec to initialize songFS
 time.sleep(2)
 
 # Parse bash printout of dir contents
 def parse_printout(txt):
     output_str = output.decode("utf-8")
 
-    # Split the output string by line breaks to get a list of directories
+    # Do string parsing on bash output
     output_lines = output_str.strip().split("\n")
     actual_directories = [line.split(maxsplit=8)[-1] for line in output_lines[1:]]
     print("Output: " + str(actual_directories))
@@ -40,16 +40,13 @@ def check_root(output):
 
 
 try:
-    # Perform operations on the mounted file system
-    # For example, list the contents of the mount point
+    # Perform operations on the mounted FS
     print("Listing contents of the mount point:")
     output = subprocess.check_output(["ls", "-l", mount_point])
     print(output.decode("utf-8"))
     check_root(output)
 
-    # Add more operations as needed to test the file system
-
 finally:
-    # Clean up: Terminate songfs process
+    # Clean up & terminate songfs process
     proc.terminate()
     proc.wait()
